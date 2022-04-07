fe0247cb6a1d4891227ed272f2c59b16621d9d34
Brayan Llanos <desarrolloit3@innpactia.com>
2022-04-06 20:46:38 -0500

    use value and use factory

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('APP_NAME') private appName: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.appName);
+    console.log(this.tasks);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -5,8 +6,24 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_NAME',
+      useValue: 'NestJS',
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const taks = await http
+          .get('https://jsonplaceholder.typicode.com/todos')
+          .toPromise();
+        return taks.data;
+      },
+      inject: [HttpService],
+    },
+  ],
 })
 export class AppModule {}
