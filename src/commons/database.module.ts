b1f0f751bd840a28c5113ca21ff2b1ec8666a909
Brayan Llanos <desarrolloit3@innpactia.com>
2022-04-12 08:01:28 -0500

    config postgres with docker and connection with typeorm

--- src/commons/database.module.ts
+++ src/commons/database.module.ts
@@ -1,7 +1,29 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../config';
 
 @Global()
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>): any => {
+        const { host, username, password, port, database } =
+          configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username,
+          password,
+          database,
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
   providers: [
     {
       provide: 'APP_DATABASE',
@@ -13,6 +35,6 @@ import { Global, Module } from '@nestjs/common';
       },
     },
   ],
-  exports: ['APP_DATABASE'],
+  exports: ['APP_DATABASE', TypeOrmModule],
 })
 export class DatabaseModule {}
--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,13 @@ export default registerAs('config', () => {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
     },
+    postgres: {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+    },
     apiKey: process.env.API_KEY,
   };
 });
